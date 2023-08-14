d054468b77a9f4bb03c55a90997f4958bda101d8
Gent Brovina <65289088+gentbrovina@users.noreply.github.com>
2023-08-14 14:47:42 +0100

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,72 @@
+# VPC Variables
+variable "region" {
+  default       = "us-central-1"
+  description   = "AWS Region"
+  type          = string
+}
+
+variable "vpc-cidr" {
+  default       = "10.0.0.0/16"
+  description   = "VPC CIDR Block"
+  type          = string
+}
+
+variable "public-subnet-1-cidr" {
+  default       = "10.0.0.0/24"
+  description   = "Public Subnet 1 CIDR Block"
+  type          = string
+}
+
+variable "public-subnet-2-cidr" {
+  default       = "10.0.1.0/24"
+  description   = "Public Subnet 2 CIDR Block"
+  type          = string
+}
+
+variable "private-subnet-1-cidr" {
+  default       = "10.0.2.0/24"
+  description   = "Private Subnet 1 CIDR Block"
+  type          = string
+}
+
+variable "private-subnet-2-cidr" {
+  default       = "10.0.3.0/24"
+  description   = "Private Subnet 2 CIDR Block"
+  type          = string
+}
+
+variable "private-subnet-3-cidr" {
+  default       = "10.0.4.0/24"
+  description   = "Private Subnet 3 CIDR Block"
+  type          = string
+}
+
+variable "private-subnet-4-cidr" {
+  default       = "10.0.5.0/24"
+  description   = "Private Subnet 4 CIDR Block"
+  type          = string
+}
+variable "database-snapshot-identifier" {
+  default = "arn:aws:rds:eu-central-1:888659321129:snapshot:gent-snapshot"
+  description = "Database snapshot ARN"
+  type = string
+
+}
+
+variable "database-instance-class" {
+  default = "db.t2.micro"
+  description = "The Database Instance Type"
+  type = string
+}
+
+variable "database-instance-indetifier" {
+    default = "postgres"
+    description = "The Database Instance Indetifier"
+    type = string
+}
+
+variable "multi-az-deployment" {
+    default = false
+    description = "Create a Secondary DB Instance"
+    type = bool
+}
